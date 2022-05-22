fb605918587fedfce16b6d3899f4f85e5c9aa048
daiwell <9782309+daiwell77@user.noreply.gitee.com>
2022-05-22 10:14:35 +0800

    getters映射companyId属性

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,11 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token:state=>state.user.token,
-  userId:state=>state.user.userInfo.userId,
-  name:state=>state.user.userInfo.name,
-  staffPhoto:state=>state.user.userInfo.staffPhoto
+  token: state => state.user.token,
+  userId: state => state.user.userInfo.userId,
+  name: state => state.user.userInfo.name,
+  staffPhoto: state => state.user.userInfo.staffPhoto,
+  companyId: state => state.user.userInfo.companyId
 
 }
 export default getters
